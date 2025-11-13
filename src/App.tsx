b5c427803d63a45d232c540a6446e2e3fb3179d3
Doug Mead <fremonade@gmail.com>
2025-11-12 21:04:55 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import ListItem from "./components/Item.tsx"
 function App() {
   return (
     <div className="text-center">
-      <h1>Doug's birthday list</h1>
+      <h1>Doug's christmas list</h1>
       <ol>
         <ListItem name="Filament!" url="https://www.amazon.com/dp/B0991P3Q25"/>
       </ol>
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -6,8 +6,7 @@ interface ListItemProps{
 function ListItem(props: ListItemProps){
     return(
         <li>
-            <h3>{props.name}</h3>
-            <a target="_blank" href={props.url}>Website</a>
+            <h3><a target="_blank" href={props.url}>{props.name}</a></h3>
         </li>
     )
 }
