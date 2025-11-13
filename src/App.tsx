131d232d36676307374bf09984b64ab0564d72f8
Doug Mead <fremonade@gmail.com>
2025-11-12 21:26:40 -0500

    Automated commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
       <ol>
         <ListItem name="Filament!" url="https://www.amazon.com/dp/B0991P3Q25"/>
         <ListItem name="Nozzles for my Ender 3!" url="https://www.amazon.com/Creality-Nozzles-3D-Printer-Extruder/dp/B093SKXHL3"/>
+        <ListItem name="Thing so my printer can print multicolor!" url="https://www.microcenter.com/product/685299/bambu-lab-ams-lite"/>
         <ListItem name="Another printer (I guess...)" url="https://www.amazon.com/Comgrow-Creality-Ender-Aluminum-220x220x250mm/dp/B07BR3F9N6"/>      
       </ol>
     </div>
